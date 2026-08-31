a28c201d2331805d0194b7b213e528933a68e547
AtLeastOneBearPlease <197534792+AtLeastOneBearPlease@users.noreply.github.com>
2026-08-31 12:10:15 -0500

    feat: Add resume object for testing display

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -1,4 +1,63 @@
-export function Resume() {
+let resumeObject = {
+    personalInformation: {
+        fullName: "Alex Walters",
+        emailAddress: "alexwalters387@gmail.com", 
+        phoneNumber: "7312203888",
+        location: "Huntingdon, TN",
+        linkedInProfile: "", 
+        githubProfile: ""
+    },
+    summary: "I am da best at everything",
+    educationInformation: {
+        schoolObjects: [
+            {
+                schoolName: "Texas University", 
+                startDate: "August 2025",
+                endDate: "",
+                degree: "Computer Science"
+            }, 
+            {
+                schoolName: "Tennessee University",
+                startDate: "August 2024", 
+                endDate: "August 2023",
+                degree: "Computer Science"
+            }
+        ]
+    },
+    workExperience: {
+        experienceObjects: [
+            {
+                companyName: "Checkr", 
+                startDate: "August 2024", 
+                endDate: "August 2026",
+                jobTitle: "Investigations",
+                responsibilities: [
+                    "Being cool",
+                    "Looking Cool"
+                ]
+            },
+            {
+                companyName: "GoodHire",
+                startDate: "August 2023",
+                endDate: "", 
+                jobTitle: "Investigations", 
+                responsibilities: [
+                    "Doing cool stuff",
+                    "Being cool and stuff", 
+                    "Also looking cool"
+                ]
+            }
+        ]
+    },
+    skills: [
+        "React", 
+        "Javascript",
+        "HTML"
+    ]
+
+}
+
+export function Resume(resumeData) {
     return (
         <div className="resume">
             Hi, I'm a resume

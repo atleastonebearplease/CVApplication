bab531044ad53b454a58bd7395e006ff8221b371
AtLeastOneBearPlease <197534792+AtLeastOneBearPlease@users.noreply.github.com>
2026-08-31 13:44:22 -0500

    feat: Add resume view with resume resumeObject
    - This means we have taken the data from the resume object and used it
    to create a simple layout in the resume preview

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -1,66 +1,58 @@
-let resumeObject = {
-    personalInformation: {
-        fullName: "Alex Walters",
-        emailAddress: "alexwalters387@gmail.com", 
-        phoneNumber: "7312203888",
-        location: "Huntingdon, TN",
-        linkedInProfile: "", 
-        githubProfile: ""
-    },
-    summary: "I am da best at everything",
-    educationInformation: {
-        schoolObjects: [
-            {
-                schoolName: "Texas University", 
-                startDate: "August 2025",
-                endDate: "",
-                degree: "Computer Science"
-            }, 
-            {
-                schoolName: "Tennessee University",
-                startDate: "August 2024", 
-                endDate: "August 2023",
-                degree: "Computer Science"
-            }
-        ]
-    },
-    workExperience: {
-        experienceObjects: [
-            {
-                companyName: "Checkr", 
-                startDate: "August 2024", 
-                endDate: "August 2026",
-                jobTitle: "Investigations",
-                responsibilities: [
-                    "Being cool",
-                    "Looking Cool"
-                ]
-            },
-            {
-                companyName: "GoodHire",
-                startDate: "August 2023",
-                endDate: "", 
-                jobTitle: "Investigations", 
-                responsibilities: [
-                    "Doing cool stuff",
-                    "Being cool and stuff", 
-                    "Also looking cool"
-                ]
-            }
-        ]
-    },
-    skills: [
-        "React", 
-        "Javascript",
-        "HTML"
-    ]
+import "./resume.css";
 
-}
+export function Resume({resumeData}) {
 
-export function Resume(resumeData) {
+    let personalInfo = resumeData.personalInformation;
+    let summary = resumeData.summary;
+    let educationInfo = resumeData.educationInformation;
+    let workExperience = resumeData.workExperience;
+    let skills = resumeData.skills;
+
+    
+//TODO: Add Keys to all objects upon creation
     return (
         <div className="resume">
-            Hi, I'm a resume
+            <div className="personal-info">
+                <h1 className="name">{personalInfo.fullName}</h1>
+                <p>{personalInfo.location} | {personalInfo.phoneNumber}</p>
+                <p><a href={personalInfo.linkedInProfile}>LinkedIn</a> | <a href={personalInfo.githubProfile}>GitHub</a> | {personalInfo.emailAddress}</p>                
+            </div>
+            <h1>Summary</h1>
+            <ul>
+                <li>{summary}</li>
+            </ul>
+            <h1>Education</h1>
+            {educationInfo.schoolObjects.map((school) => {
+                return <>
+                    <h3>{school.schoolName}</h3>
+                    <p>{school.startDate} - {school.endDate ? school.endDate : "Present"}</p>
+                    <h4>{school.degree}</h4>
+                    <ul>
+                        {school.achievements.map((ach) => {
+                            return (<li>{ach}</li>);
+                        })}
+                    </ul>
+                </>
+            })}
+            <h1>Work Experience</h1>
+            {workExperience.workObjects.map((workObj) => {
+                return <>
+                    <h3>{workObj.companyName}</h3>
+                    <p>{workObj.startDate} - {workObj.endDate ? workObj.endDate : "Present"}</p>
+                    <h4>{workObj.jobTitle}</h4>
+                    <ul>
+                        {workObj.responsibilities.map((rsp) => {
+                            return (<li>{rsp}</li>);
+                        })}
+                    </ul>
+                </>
+            })}
+            <h1>Skills</h1>
+            <ul>
+                {skills.map((skill) => {
+                    return (<li>{skill}</li>)
+                })}
+            </ul>
         </div>
     )
 }
---
+++ src/resumeObject.js
@@ -0,0 +1,66 @@
+export const resumeObject = {
+    personalInformation: {
+        fullName: "Alex Walters",
+        emailAddress: "alexwalters387@gmail.com", 
+        phoneNumber: "7312203888",
+        location: "Huntingdon, TN",
+        linkedInProfile: "", 
+        githubProfile: ""
+    },
+    summary: "I am da best at everything",
+    educationInformation: {
+        schoolObjects: [
+            {
+                schoolName: "Texas University", 
+                startDate: "August 2025",
+                endDate: "",
+                degree: "Computer Science",
+                achievements: [
+                    "Was super cool",
+                    "Knew super cool people"
+                ]
+            }, 
+            {
+                schoolName: "Tennessee University",
+                startDate: "August 2024", 
+                endDate: "August 2023",
+                degree: "Computer Science",
+                achievements: [
+                    "Being the best",
+                    "Graduated Early"
+                ]
+            }
+        ]
+    },
+    workExperience: {
+        workObjects: [
+            {
+                companyName: "Checkr", 
+                startDate: "August 2024", 
+                endDate: "August 2026",
+                jobTitle: "Investigations",
+                responsibilities: [
+                    "Being cool",
+                    "Looking Cool"
+                ]
+            },
+            {
+                companyName: "GoodHire",
+                startDate: "August 2023",
+                endDate: "", 
+                jobTitle: "Investigations", 
+                responsibilities: [
+                    "Doing cool stuff",
+                    "Being cool and stuff", 
+                    "Also looking cool"
+                ]
+            }
+        ]
+    },
+    skills: [
+        "React", 
+        "Javascript",
+        "HTML"
+    ]
+
+}
